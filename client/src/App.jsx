9993393e4ec9f68e521bffb946f46af9a03702ff
Runster91 <Guillermo.Casillas.l@outlook.com>
2023-10-12 22:40:06 -0400

    licht

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './assets/components/Header';
+import Footer from './assets/components/Footer';
+import Reservation from './assets/components/Reservation';
+
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Reservation />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/assets/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Nihon no Tabemono </h1>
+      <h1>日本の食べ物</h1>
+    </header>
+  );
+};
+
+
+export default Header;
